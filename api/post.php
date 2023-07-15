87e53b24564c9b586e179cc534143bac0ed8ebf4
Kordelor <32760447+Kordelor@users.noreply.github.com>
2023-07-15 17:23:55 +0200

    Update post.php

--- api/post.php
+++ api/post.php
@@ -6,14 +6,17 @@
                 ['type' => 'bus','number'=>'', 'from' => 'Barcelona', 'to' => 'Gerona Airport', 'seat'=>''],
                 ['type' => 'train','number'=>'78A', 'from' => 'Madrid', 'to' => 'Barcelona', 'seat'=>'45b']
             );  
-
+    $dataString = '[{"type":"flight","number":"SK22","from":"Stockholm","to":"New York JFK","seat":"7B","gate":"45b","ticketCounter":""},
+                    {"type":"flight","number":"SK455","from":"Gerona Airport","to":"Stockholm","seat":"3A","gate":"45b","ticketCounter":"344"},
+                    {"type":"bus","number":"","from":"Barcelona","to":"Gerona Airport","seat":""},
+                    {"type":"train","number":"78A","from":"Madrid","to":"Barcelona","seat":"45b"}]';  
     // Envoie de la requette
     $options = array(
         'http' => array(
             'header' => "content-type: application/x-www-form-urlencoded",
             'method' => 'POST',
             //'content' => http_build_query($data)
-            'content' => 'data=[{"type":"flight","number":"SK22","from":"Stockholm","to":"New York JFK","seat":"7B","gate":"45b","ticketCounter":""},{"type":"flight","number":"SK455","from":"Gerona Airport","to":"Stockholm","seat":"3A","gate":"45b","ticketCounter":"344"},{"type":"bus","number":"","from":"Barcelona","to":"Gerona Airport","seat":""},{"type":"train","number":"78A","from":"Madrid","to":"Barcelona","seat":"45b"}]'
+            'content' => 'data='.$dataString
         )
     );
     $context = stream_context_create($options, $data );
